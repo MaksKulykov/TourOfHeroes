f9a7b758f02073facaaad3896d0bb2fb776b3ecd
MaksKulikov <kulykov.maks@gmail.com>
2017-02-25 23:50:57 +0200

    added routing module

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -6,18 +6,9 @@ import 'rxjs/add/operator/switchMap';
 import { HeroService } from './hero.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-        <div *ngIf="hero">
-           <h2>{{hero.name}} details!</h2>
-           <div><label>id: </label>{{hero.id}}</div>
-           <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
-           </div>
-        </div>
-        <button (click)="goBack()">Back</button>
-    `,
+  templateUrl: './hero-detail.component.html',
 })
 
 export class HeroDetailComponent implements OnInit {
