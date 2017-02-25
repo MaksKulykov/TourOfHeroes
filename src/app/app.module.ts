ebfc3f9a19baa55e38b37496c16afa7fd7719e96
MaksKulikov <kulykov.maks@gmail.com>
2017-02-25 21:26:48 +0200

    app component as a shell for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,29 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from  './heroes.component';
+import { HeroService } from "./hero.service";
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+    {
+      path: 'heroes',
+      component: HeroesComponent
+    }
+  ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  bootstrap:    [ AppComponent ],
+  providers: [
+    HeroService
+  ]
 })
 export class AppModule { }
